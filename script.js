655029a4c1aa9d830b11da875b4d73f134c0db8d
clu23 <clement.lulurga@orange.fr>
2023-09-17 18:59:06 +0200

    making the grid directly in the html file and some changes in functions

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
+
+/*
 const grid = document.getElementById('container-grid');
-
-
-
-const Gameboard=(()=>{
-    grid.style.gridTemplateColumns = `repeat(${3}, 1fr)`
-    grid.style.gridTemplateRows = `repeat(${3}, 1fr)`
+grid.style.gridTemplateColumns = `repeat(${3}, 1fr)`
+grid.style.gridTemplateRows = `repeat(${3}, 1fr)`
   
     for (let i = 0; i < 9; i++) {
         const gridElement = document.createElement('div')
         gridElement.classList.add('grid-element')
         grid.appendChild(gridElement)
     }
-    let cells = document.querySelectorAll('.grid-element');
-    return{cells};
+let cells = document.querySelectorAll('.grid-element');
+*/
+
+
+/**
+ * This module stores the game board information
+ */
+
+const Gameboard=(()=>{
+    let _board=new Array(9);
+    const getField = (num) => _board[num];
+
+    const clear = () => {
+        for (let i = 0; i < _board.length; i++) {
+            _board[i] = undefined;
+        }
+    }
+
+    
+    return{
+        getField,
+        clear
+    };
 })();
 
 
@@ -22,4 +41,3 @@ const Player=(name,score) =>{
 
 
 
-console.log(Gameboard.cells)
